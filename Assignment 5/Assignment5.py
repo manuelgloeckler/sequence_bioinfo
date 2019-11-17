c9fd6bc4c8c81611bcd1c297f9047c2a183538dd
manuelgloeckler <38903899+manuelgloeckler@users.noreply.github.com>
2019-11-17 15:37:59 +0100

    matrix

--- Assignment 5/Assignment5.py
+++ Assignment 5/Assignment5.py
@@ -1,5 +1,6 @@
 from FastA import *
 import numpy as np
+from Bio import pairwise2
 
 def parse_score(path):
     file = open(path, 'r')
@@ -8,10 +9,10 @@ def parse_score(path):
     i = 0
     for x in file:
         if i == 0:
-            alphabet = x.split("\t")
+            alphabet = x.split()
             alphabet = [x.strip() for x in alphabet]
         else:
-            row = x.split("\t")
+            row = x.split()
             subst = row[0]
             del row[0]
             for j in range(len(row)):
@@ -19,82 +20,13 @@ def parse_score(path):
         i+=1
     return score_dict
 
-def _tracking_guide(diag, top, left):
-    winner = max(diag, top, left)
-    # diag as 0, top 1, left 2
-    if winner == diag:
-        return winner, 0
-    elif winner == top:
-        return winner, 1
-    elif winner == left:
-        return winner, 2
-
-def matrix_builder(x_seq, y_seq, score):
-    ncol = len(x_seq) + 1
-    nrow = len(y_seq) + 1
-    matF = [i[:] for i in [[0] * ncol] * nrow]
-    matT = [i[:] for i in [[0] * ncol] * nrow]
-
-    # Fill table with values
-    for i in range(nrow):
-        for j in range(ncol):
-            if i == 0:
-                matF[i][j] = j * (- score.gap)
-                matT[i][j] = 1
-            elif j == 0:
-                matF[i][j] = i * (- score.gap)
-                matT[i][j] = 2
-            else:
-                s = score.match if x_seq[j - 1] == y_seq[i - 1] else score.mismatch
-                diag = matF[i - 1][j - 1] + s
-                top = matF[i - 1][j] - score.gap
-                left = matF[i][j - 1] - score.gap
-                matF[i][j], matT[i][j] = _tracking_guide(diag, top, left)
-    score.best_score = matF[nrow - 1][ncol - 1]
-    print("Best alignment score: {}".format(score.best_score))
-    return matT
-
-
-def traceback_to_alignment(matT, x_seq, y_seq):
-    x_aligned = ""
-    y_aligned = ""
-    i, j = len(y_seq), len(x_seq)
-    while i and j:
-        if matT[i][j] == 0:
-            x_aligned += x_seq[j - 1]
-            y_aligned += y_seq[i - 1]
-            i -= 1
-            j -= 1
-        elif matT[i][j] == 1:
-            x_aligned += "-"
-            y_aligned += y_seq[i - 1]
-            i -= 1
-        elif matT[i][j] == 2:
-            x_aligned += x_seq[j - 1]
-            y_aligned += "-"
-            j -= 1
-    while i:
-        x_aligned += "-"
-        y_aligned += y_seq[i - 1]
-        i -= 1
-    while j:
-        y_aligned += "-"
-        x_aligned += x_seq[j - 1]
-        j -= 1
-    return x_aligned[::-1], y_aligned[::-1]
-
-
-def run_nw(x_seq, y_seq, score):
-    matT = matrix_builder(x_seq, y_seq, score)
-    return traceback_to_alignment(matT, x_seq, y_seq)
-
 def hamming_dist(str1, str2):
-    assert (len(str1) != len(str2))
+    #assert (len(str1) != len(str2))
     score = 0
     for i in range(len(str1)):
         if str1[i] != str2[i] and (str1[i] != "-" or str1[i] != "-"):
             score +=1
-    return score
+    return score/len(str1)
 
 def kimmura_dist(str1, str2):
     hamm_dist = hamming_dist(str1, str2)
@@ -105,12 +37,37 @@ def distance_matrix(strs):
     dist_matrix = np.zeros((n,n))
     for i in range(n):
         for j in range(n):
-            dist_matrix[i,j] = kimmura_dist(run_nw(strs[i], strs[j])[1:])
+            dist_matrix[i,j] = kimmura_dist(run_nw(strs[i], strs[j])[:2])
     return dist_matrix
 
+def pair_guided_alignment(align1:list, align2:list, score):
+    seq1 = np.random.choice(align1)
+    seq2 = np.random.choice(align2)
+    new_alignment = pairwise2.align.globaldx(seq1, seq2, score)[0]
+    new_seq1 = new_alignment[0]
+    new_seq2 = new_alignment[1]
+
+    gaps1 = np.where(np.array(list(seq1)) == "-")[0]
+    gaps2 = np.where(np.array(list(seq2)) == "-")[0]
+    newgaps1 =  np.where(np.array(list(seq1)) == "-")[0]
+
+
+
+
+
+    for i in range(len(seq1)):
+
+
 def main():
-    blossumMatrix = parse_score('./BLOSUM62.txt')
-    headers, sequences = read_sequence('./BB11007_unaligned.fasta')
+    blossumMatrix = parse_score('./blosum62.txt')
+    print(blossumMatrix)
+    #headers, sequences = read_sequence('./BB11007_unaligned.fasta')
+    align = pairwise2.align.globaldx("A-GG", "G--GA", blossumMatrix)
+    seq1 = align[0][0]
+    seq2 = align[0][1]
+    print(seq1,seq2)
+    print(hamming_dist(seq1, seq2))
+    print(kimmura_dist(seq1,seq2))
 
 
 if __name__ == "__main__":
